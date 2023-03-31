b7da12cde01defa678cdbbe80d5d9778ea5884c6
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-03-31 00:21:03 -0300

    adding css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar.
         </p>
         <a
           className="App-link"
@@ -15,6 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
           Learn React
         </a>
       </header>
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    // chamado de JSX
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="imagem do banner principal" />
+        </header>
+
+    )
+}
+
+export default Banner;
