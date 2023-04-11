f6c2f86eb933f3d1b2289210681fd56d94b457bb
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-04-11 17:43:12 -0300

    rodape finalizado

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,15 +3,22 @@ import './Rodape.css'
 const Rodape = (props) => {
     return (
         <footer className='rodape'>
-            <div className='redes'> 
-                <img src="/images/fb.png" alt='ícone do facebook' />
-                <img src="/images/tw.png" alt='ícone do twitter' />
-                <img src="/images/ig.png" alt='ícone do instagram' />
-                
+            <div className='redes'>
+                <a href='https://www.linkedin.com/in/carlos-vinícius-bezerra-sampaio-b6a302254/' target='_blank'>
+                    <img src="/images/fb.png" alt='ícone do facebook' />
+                </a>
+                <a href='https://www.linkedin.com/in/carlos-vinícius-bezerra-sampaio-b6a302254/' target='_blank'>
+                    <img src="/images/tw.png" alt='ícone do twitter' />
+                </a>
+                <a href='https://www.linkedin.com/in/carlos-vinícius-bezerra-sampaio-b6a302254/' target='_blank'>
+                    <img src="/images/ig.png" alt='ícone do instagram' />
+                </a>
+
+
             </div>
             <img src="/images/logo.png" className='logoOrgano' alt='logo da marca' />
             <div className='autor'>
-                <h4>Desenvolvido por Carlos Vinícius</h4>
+               <a href='https://github.com/Carl-Vini'><h4>Desenvolvido por Carlos Vinícius</h4></a> 
             </div>
         </footer>
     )
